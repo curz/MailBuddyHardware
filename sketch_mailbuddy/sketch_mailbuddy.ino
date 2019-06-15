5d8471522eeeb744c02213d75886cd3044178689
Sdiep <imerokus@yahoo.de>
2019-06-15 16:35:10 +0200

    First push

---
+++ sketch_mailbuddy/sketch_mailbuddy.ino
@@ -0,0 +1,177 @@
+#include <stdlib.h>
+#include <SoftwareSerial.h>// import the serial library
+
+#define CALIBRATION_SAMPLE_SIZE 1000
+#define BLUETOOTH_MESSAGE_DELAY 100
+
+String INFO_MAIL_STATUS = "mail_status";
+String INFO_HALL_SENSOR_READING = "hall_reading";
+String INFO_GENERAL_OUTPUT = "general_output";
+String INFO_CONNECTION_STATUS = "connection_status";
+
+float CALIBRATION_STEP_DURATION = 1000.0; // in ms
+float CALIBRATION_TOTAL_DURATION = 5000.0; // in ms
+
+int hallSensorPin = A5;
+int ledPin = 13;
+int hallData = 0;
+int hallDataStandard;
+int sensorTolerance = 2;
+unsigned long currentTimeMillis = 0;
+int updateRate = 100;
+bool opened = false;
+bool mailPresent = false;
+
+//Configure bluetooth
+char REQUEST_MAIL_STATUS = '1';
+char REQUEST_START_HALL_READ = '2';
+char REQUEST_STOP_HALL_READ = '3';
+char REQUEST_RESET = '4';
+char REQUEST_TOGGLE_LED = '5';
+
+SoftwareSerial btModule(2,3);
+
+int btData;
+bool sendingHallData = false;
+unsigned long lastHallDataMillis = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  btModule.begin(9600);
+  calibrate();
+}
+
+void loop() {
+  if((unsigned long)(millis() - currentTimeMillis)> updateRate){ // Sleep for fixed duration
+    currentTimeMillis = millis();
+    
+    hallData = analogRead(hallSensorPin);
+    //btModule.println(analogRead(hallSensorPin));
+
+    checkBluetooth();
+    checkLid();
+    if(sendingHallData){
+      sendHallData();
+    }
+  }
+
+  //Debugging
+  if(Serial.available()){
+    btModule.println(Serial.read());
+  }
+  if(btModule.available()){
+    Serial.println(btModule.read());
+  }
+}
+
+void sendHallData(){
+  if((unsigned long)(millis() - lastHallDataMillis) > 500){
+    lastHallDataMillis = millis();
+    btModule.println(INFO_HALL_SENSOR_READING + ":" + "Hall sensor reads: " + hallData);
+    delay(100);
+  }
+}
+
+void checkBluetooth(){
+  if(btModule.available()){
+    btData = btModule.read();
+    if(btData==REQUEST_MAIL_STATUS){
+//      btModule.println(INFO_MAIL_STATUS, mailPresent);
+      delay(100);
+    }else if(btData==REQUEST_START_HALL_READ){
+      sendingHallData = true;
+    }else if(btData==REQUEST_STOP_HALL_READ){
+      sendingHallData = false;
+      lastHallDataMillis = 0;
+    }else if(btData==REQUEST_TOGGLE_LED){
+      digitalWrite(ledPin, !digitalRead(ledPin));
+      btModule.println("LED toggled!");
+      delay(100);
+    }
+  }
+  btModule.flush();
+}
+
+void checkLid(){
+    // Start with closed lid
+  if(abs(hallDataStandard-hallData) > sensorTolerance && !opened){ // if lid was opened aka hall sensor detected change
+    digitalWrite(ledPin, HIGH);
+      opened = true;
+      mailPresent = true;
+  }
+}
+void calibrate() {
+  int startState[CALIBRATION_SAMPLE_SIZE];
+  int currentState;
+  int smallestState;
+  int biggestState;
+  unsigned long sumStates= 0;
+  int countdown = CALIBRATION_TOTAL_DURATION/1000;
+
+  Serial.println("Calibrating...");
+  Serial.println(countdown);
+
+  btModule.println(INFO_GENERAL_OUTPUT + ":" + "Calibrating...");
+  btModule.println(INFO_GENERAL_OUTPUT + ":" + countdown);
+  delay(100);
+
+  for(int i = 0, k=0 ; i < CALIBRATION_SAMPLE_SIZE; i++, k++){
+    if(currentTimeMillis == 0){
+      currentTimeMillis = millis();
+    }
+    
+    currentState = analogRead(hallSensorPin);
+    if(i == 0){
+      smallestState = currentState;
+      biggestState = currentState;
+    }
+    
+    startState[i] = currentState;
+    sumStates += currentState;
+
+    // keep track of smallest and biggest occuring sensor values
+    if(currentState < smallestState) {
+      smallestState = currentState;
+    } else if (currentState > biggestState) {
+      biggestState = currentState;
+    }
+
+    if(k >= CALIBRATION_SAMPLE_SIZE*CALIBRATION_STEP_DURATION/CALIBRATION_TOTAL_DURATION){ // Check if you have enough samples for the defined step duration
+      while(millis() < currentTimeMillis + CALIBRATION_STEP_DURATION){} // Wait until the current step ended if necessary
+      countdown--;
+      Serial.println(countdown);
+      btModule.println(INFO_GENERAL_OUTPUT + ":" + countdown);
+      currentTimeMillis = 0; // reset the timestamp
+      k = 0; // reset counter for one step
+    }
+
+    digitalWrite(ledPin, LOW); // start with turned off LED
+  }
+  int deviation = biggestState - smallestState;
+  if(deviation > sensorTolerance){
+      sensorTolerance = deviation;
+  }
+
+  hallDataStandard = sumStates/CALIBRATION_SAMPLE_SIZE;
+  
+  Serial.println((String)"Collected samples: " + CALIBRATION_SAMPLE_SIZE);
+  Serial.println((String)"Collected over a duration of: " + CALIBRATION_TOTAL_DURATION);
+  Serial.println((String)"Calibration value: " + hallDataStandard);
+  Serial.println((String)"Detected deviation: " + deviation);  
+  Serial.println((String)"Chosen deviation: " + sensorTolerance);  
+  
+  btModule.println(INFO_GENERAL_OUTPUT + ":" + "Collected samples: " + CALIBRATION_SAMPLE_SIZE);
+  delay(100);
+  btModule.println(INFO_GENERAL_OUTPUT + ":" + "Collected over a duration of: " + CALIBRATION_TOTAL_DURATION);
+  delay(100);
+  hallDataStandard = sumStates/CALIBRATION_SAMPLE_SIZE;
+  btModule.println(INFO_GENERAL_OUTPUT + ":" + "Calibration value: " + hallDataStandard);
+  delay(100);
+  btModule.println(INFO_GENERAL_OUTPUT + ":" + "Detected deviation: " + deviation);  
+  delay(100);
+  btModule.println(INFO_GENERAL_OUTPUT + ":" + "Chosen deviation: " + sensorTolerance);  
+  delay(3000);
+}
+
+  
